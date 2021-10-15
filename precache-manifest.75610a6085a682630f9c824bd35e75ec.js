8a258733ed69f31861286ce15e6996541dfcc534
ajing2020 <1277483876@qq.com>
2021-10-15 09:03:07 +0800

    deploy

---
+++ precache-manifest.75610a6085a682630f9c824bd35e75ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5b5052a8749462a789b",
+    "url": "/Project-Huahua-website/css/app.b43d8c24.css"
+  },
+  {
+    "revision": "5c928dd169ab98d2e506b52e5e76c704",
+    "url": "/Project-Huahua-website/index.html"
+  },
+  {
+    "revision": "d5b5052a8749462a789b",
+    "url": "/Project-Huahua-website/js/app.8ab28125.js"
+  },
+  {
+    "revision": "4209214145df254ea2b8",
+    "url": "/Project-Huahua-website/js/chunk-vendors.c5290bfd.js"
+  },
+  {
+    "revision": "7971d0d78a0e5c96df2e3eb600f2aa36",
+    "url": "/Project-Huahua-website/manifest.json"
+  },
+  {
+    "revision": "7570f26c23eb7adf157e262f00118853",
+    "url": "/Project-Huahua-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Project-Huahua-website/robots.txt"
+  }
+]);
